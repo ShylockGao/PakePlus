087f6e079e5a5b666ad0bc946e75df81efc5065d
Shylock Gao <gaohaonan01@gmail.com>
2025-03-11 17:27:31 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,5 +1,17 @@
-console.log('custom.js ----')
+console.log(
+    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
+    'color:orangered;font-weight:bolder'
+)
 
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-})
+const hookClick = (e) => {
+    e.preventDefault()
+    const origin = e.target.closest('a')
+    if (origin && origin.href) {
+        location.href = origin.href
+    }
+}
+
+function init_desktop_air(){
+    localStorage.setItem("modelingAppType","TRURI");
+}
+init_desktop_air()
